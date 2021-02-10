4a510d58e9d8ed24c734a6f672c8d4a7b6b57711
Ashish <marisetty19102@iiitnr.edu.in>
2021-02-10 15:44:34 +0530

    Updated Cartoonify code ;P

--- modules/Cartoon/main.py
+++ modules/Cartoon/main.py
@@ -2,21 +2,33 @@ import cv2
 import numpy as np
 import streamlit as st
 
-def cartoonify(input_img,params = []):
+def edged_mask(img, line_size, blur_value):
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray_blur = cv2.medianBlur(gray, blur_value)
+  edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_size, blur_value)
+  return edges
+
+def color_quantization(img, k):
+# Transform the image
+  data = np.float32(img).reshape((-1, 3))
+# Determine criteria
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+
+# Implementing K-Means
+  ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+  center = np.uint8(center)
+  result = center[label.flatten()]
+  result = result.reshape(img.shape)
+  return result
+
+def cartoonify(img,params = []):
     if params == []:
-        #Default parameters
-        d,sigmaValue,blockSize,c = 30,300,13,5
+        line_size,blur_value,total_color = 7,7,11
     else:
-        d,sigmaValue,blockSize,c = params
+        line_size,blur_value,total_color = params
 
-    img_color = cv2.bilateralFilter(input_img, d, sigmaValue,sigmaValue)
-
-    # prepare edges
-    img_edges = cv2.cvtColor(input_img, cv2.COLOR_RGB2GRAY)
-    img_edges = cv2.adaptiveThreshold(cv2.medianBlur(img_edges,17),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,blockSize,c)
-    img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-    # combine color and edges
-    final_img = cv2.bitwise_and(img_color, img_edges)
-    #kernel = np.ones((7,7),dtype = np.uint8)
-    #final_img = cv2.morphologyEx(final_img,cv2.MORPH_CLOSE,kernel)
-    return final_img
+    edges = edged_mask(img, line_size, blur_value)
+    quantized_img = color_quantization(img, total_color)
+    blurred = cv2.bilateralFilter(quantized_img, d=7, sigmaColor=200,sigmaSpace=200)
+    cartooned_img = cv2.bitwise_and(blurred, blurred, mask=edges)
+    return cartooned_img
